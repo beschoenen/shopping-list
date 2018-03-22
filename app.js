30c023a5552bf608fadfe5cc9c3bfb1a6ae8c410
Kevin Richter <me@kevinrichter.nl>
2018-03-22 17:14:25 +0100

    Add meta tags; multi-size logo

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const path = require('path');
 const sassMiddleware = require('node-sass-middleware');
 const mongoose = require('mongoose');
-const handlebars = require('hbs');
 
 const app = express();
 
@@ -23,11 +22,5 @@ app.use('/public', express.static(path.join(__dirname, 'public')));
 app.use('/', require('./routes/index'));
 
 app.use((req, res) => res.redirect('/'));
-
-handlebars.registerHelper('ifAnyChecked', (value, options) => {
-  if (value.filter(e => e.checked).length > 0) {
-    return options.fn(this);
-  }
-});
 
 module.exports = app;
