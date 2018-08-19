8c45d662ec41faf2dc1eb09e557c41eabc26418b
Kevin Richter <me@kevinrichter.nl>
2018-08-19 11:57:37 +0200

    Simplify routes, shorten title

--- routes/index.js
+++ routes/index.js
@@ -4,24 +4,16 @@ const router = express.Router();
 
 const Room = mongoose.model('Room');
 
-router.get('/', function (req, res) {
-  Room.findOneAndUpdate(
-    { name: 'default' },
-    { name: 'default' },
-    { upsert: true, new: true, setDefaultsOnInsert: true }
-  ).exec().then(room => {
-    res.render('index', { room: room });
-  });
-});
+router.get('/:room?', async function (req, res) {
+  const name = req.params.room || 'default';
 
-router.get('/:room', function (req, res) {
-  Room.findOneAndUpdate(
-    { name: req.params.room },
-    { name: req.params.room },
+  const room = await Room.findOneAndUpdate(
+    { name },
+    { name },
     { upsert: true, new: true, setDefaultsOnInsert: true }
-  ).exec().then(room => {
-    res.render('index', { room: room });
-  });
+  ).exec();
+
+  res.render('room', { room });
 });
 
 module.exports = router;
