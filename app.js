481fdc3473033c14540f79be5342b92812723538
Kevin Richter <k.richter1994@gmail.com>
2023-03-06 14:43:17 +0100

    Run startup in try-catch & fix mongoose deprecation

--- app.js
+++ app.js
@@ -8,22 +8,28 @@ const mongoHost = process.env.MONGO_HOST || '127.0.0.1';
 const mongoPort = process.env.MONGO_PORT || 27017;
 const mongoDB = process.env.MONGO_DB || 'shopping-list';
 
-mongoose.Promise = global.Promise;
-mongoose.connect(`mongodb://${mongoHost}:${mongoPort}/${mongoDB}`).catch(error => {
-  console.log(`MongoDB connection error. Please make sure MongoDB is running. ${error}`);
+try {
+  mongoose.Promise = global.Promise;
+  mongoose.set('strictQuery', false);
+  mongoose.connect(`mongodb://${mongoHost}:${mongoPort}/${mongoDB}`).catch(error => {
+    console.log(`MongoDB connection error. Please make sure MongoDB is running. ${error}`);
+    process.exit(1);
+  }).then(() => console.log("Connected to MongoDB."));
+
+  require('./models');
+
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'hbs');
+
+  app.use('/public', express.static(path.join(__dirname, 'public')));
+  app.use('/public', require('./routes/dependencies'));
+  app.use('/version', require('./routes/version'));
+  app.use('/', require('./routes/index'));
+
+  app.use((req, res) => res.redirect('/'));
+} catch (e) {
+  console.log(e);
   process.exit(1);
-}).then(() => console.log("Connected to MongoDB."));
-
-require('./models');
-
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-
-app.use('/public', express.static(path.join(__dirname, 'public')));
-app.use('/public', require('./routes/dependencies'));
-app.use('/version', require('./routes/version'));
-app.use('/', require('./routes/index'));
-
-app.use((req, res) => res.redirect('/'));
+}
 
 module.exports = app;
